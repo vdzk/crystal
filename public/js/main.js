b90fecf88da11c2ba9b02288bb2e5a149dd43b32
Dante <dante@thisislanguage.com>
2015-10-26 22:37:20 +0000

    modularizing backbone

---
+++ public/js/main.js
@@ -0,0 +1,45 @@
+/*global require*/
+'use strict';
+
+// Require.js allows us to configure shortcut alias
+require.config({
+	// The shim config allows us to configure dependencies for
+	// scripts that do not call define() to register a module
+	shim: {
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: [
+				'underscore',
+				'jquery'
+			],
+			exports: 'Backbone'
+		},
+		backboneLocalstorage: {
+			deps: ['backbone'],
+			exports: 'Store'
+		}
+	},
+	paths: {
+		jquery: '/js/libs/jquery',
+		underscore: '/js/libs/underscore',
+		backbone: '/js/libs/backbone',
+		backboneLocalstorage: '/js/libs/backbone.localStorage',
+		text: '/js/libs/text'
+	}
+});
+
+require([
+	'backbone',
+	'views/app',
+	'routers/router'
+], function (Backbone, AppView, Workspace) {
+	/*jshint nonew:false*/
+	// Initialize routing and start Backbone.history()
+	new Workspace();
+	Backbone.history.start();
+
+	// Initialize the application view
+	new AppView();
+});
---
+++ public/js/views/app.js
@@ -0,0 +1,56 @@
+define([
+	'jquery',
+	'underscore',
+	'backbone',
+	'collections/arguments',
+	'views/argument',
+], function ($, _, Backbone, Arguments, ArgumentView) {
+
+	// Our overall **AppView** is the top-level piece of UI.
+	var AppView = Backbone.View.extend({
+		el: '#social-movements',
+		initialize: function () {
+			this.input = this.$('#new-argument');
+			Arguments.on('add', this.addOne, this);
+			Arguments.on('reset', this.addAll, this);
+			Arguments.fetch();
+		},
+		events: {
+			'click #add-new-argument': 'createArgumentOnBtnClick'
+		},
+		createArgumentOnBtnClick: function(event) {
+			event.preventDefault();
+			if (!this.input.val().trim()) {
+				return false;
+			}
+			Arguments.create(this.newAttributes());
+			this.input.val('');
+		},
+		addOne: function(argument){
+			var view = new app.ArgumentView({model: argument});
+			$('#argument-list').append(view.render().el);
+		},
+		addAll: function(){
+			this.$('#argument-list').html('');
+			switch(window.filter){
+				case 'top':
+					_.each(Arguments.top(), this.addOne);
+					break;
+				case 'new':
+					_.each(Arguments.zero_confidence(), this.addOne);
+					break;
+				default:
+					Arguments.each(this.addOne, this);
+					break;
+			}
+		},
+		newAttributes: function(){
+			return {
+				title: this.input.val().trim(),
+				confidence: 0
+			}
+		}
+	});
+
+	return AppView;
+});
