a92f5f423acf0f9ede88d640745b93c593d30c0c
Dante <dante@thisislanguage.com>
2015-12-21 17:44:40 +0000

    set up ArangoDB

---
+++ init_db.js
@@ -0,0 +1,30 @@
+var db = require('arangojs')();
+
+db.createDatabase('crystal').then(
+	() => {
+		console.log('Database created.')
+	},
+	err => {
+		if (err.message == 'duplicate name') {
+			console.log('Database already exists.');
+		} else {
+			throw err;
+		}
+	}
+).then(
+	() => {
+		db.useDatabase('crystal');
+		console.log('Switched to crystal database.');
+		argumnets = db.collection('argumnets');
+		return argumnets.create();
+	}
+).then(
+	() => console.log('Collection created'),
+	err => {
+		if (err.message == 'cannot create collection: duplicate name') {
+			console.log('Collection already exists.');
+		} else {
+			throw err;
+		}
+	}
+);
--- server.js
+++ server.js
@@ -3,10 +3,37 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
+var db = require('arangojs')();
+
+//Set up the database and a collection
+db.useDatabase('crystal');
+argumnets = db.collection('argumnets');
+
+//Running some test queries
+doc = {
+	_key: 'firstDocument',
+	a: 'foo',
+	b: 'bar',
+	c: Date()
+};
+argumnets.save(doc).then(
+	meta => {
+		console.log('Document saved:', meta._rev);
+		return argumnets.document('firstDocument');
+	},
+	err => console.error('Failed to save document:', err)
+).then(
+	doc => {
+		console.log('Document:', JSON.stringify(doc, null, 2));
+		return argumnets.remove('firstDocument');
+	},
+	err => console.error('Failed to fetch document:', err)
+).then(
+  () => console.log('Document removed'),
+  err => console.error('Failed to remove document', err)
+);
 
 //Set express routes
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(favicon(__dirname + '/public/img/global/pink_crystal.ico'));
-
-console.log(__dirname + '/public/img/global/pink_crystal.ico');
-app.listen(80);
+app.listen(3000);
